2a63bc9e55ea98600939021d1800112b7bedcbec
Leon Chin <leonchin499@gmail.com>
2016-02-27 01:02:04 +0800

    updated login

---
+++ public_html/js/login.js
@@ -0,0 +1,49 @@
+function loginFunction() {
+    $.post({
+        url: "/login",
+        data: JSON.stringify({
+            username: $("#username").val(),
+            password: $("#password").val()
+        }),
+        success: function(data, response) {
+            if(response == "success") {
+                connectionSuccess(data);
+            } else {
+                loginNetworkError();
+            }
+        },
+        contentType: "application/json"
+    });
+}
+
+function connectionSuccess(data) {
+    var serverResponse = JSON.parse(data);
+    if(serverResponse.success == true) {
+        // user has successfully logged in
+        if(serverResponse.userType == "patron") {
+            
+        } else if(serverResponse.userType == "entrepreneur") {
+            
+        } else if(serverResponse.userType == "admin") {
+            
+        } else {
+            // random error
+        }
+        
+    } else {
+        // print serverResponse.errorMessage
+        userDoesNotExist();
+    }
+}
+
+// function for bad network error
+function loginNetworkError() {
+    console.log("network error");
+}
+
+// user does not exist
+function userDoesNotExist() {
+    
+}
+
+document.getElementById('loginButton').onclick = function() {loginFunction()};
