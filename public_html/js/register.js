1bc19415965d7baeed0c672aa28e448a4ceeb8be
Leon Chin <leonchin499@gmail.com>
2016-03-06 15:27:56 +0800

    updated login and register page

--- public_html/js/register.js
+++ public_html/js/register.js
@@ -16,42 +16,42 @@ function patronRegistration() {
 }
 
 function createPatronRegistrationHTML() {
-  var str = `<div class="input-container">
-                <input type="text" id="patronEmail" required="required" />
-                <label for="userEmail">Email</label>
-                <div class="bar"></div>
-            </div>
-            <div class="input-container">
-                <input type="password" id="patronPassword" required="required" />
-                <label for="patronPassword">Password</label>
-                <div class="bar"></div>
-            </div>
-            <div class="input-container">
-                <input type="text" id="name" required="required"/>
-                <label for="name">Name</label>
-                <div class="bar"></div>
-            </div>
-            <div class="button-container">
-                <button id="patron-register-go"><span>Register</span></button>
-            </div>`;
-
-  return str;
-}
-
-function createEntreRegistrationHTML() {
   var emailHTML = `<div class="input-container">
-                      <input type="text" id="userEmail" required="required" />
+                      <input type="text" id="patronEmail" required="required" />
                       <label for="userEmail">Email</label>
                       <div class="bar"></div>
                   </div>`;
   var passwordHTML = `<div class="input-container">
-                        <input type="password" id="password" required="required" />
-                        <label for="password">Password</label>
+                          <input type="password" id="patronPassword" required="required" />
+                          <label for="patronPassword">Password</label>
+                          <div class="bar"></div>
+                      </div>`;
+  var nameHTML = `<div class="input-container">
+                      <input type="text" id="name" required="required"/>
+                      <label for="name">Name</label>
+                      <div class="bar"></div>
+                  </div>`;
+  var registerButton = `<div class="button-container">
+                            <button onclick="patronRegister()"><span>Register</span></button>
+                        </div>`;
+
+  return emailHTML + passwordHTML + nameHTML + registerButton;
+}
+
+function createEntreRegistrationHTML() {
+  var emailHTML = `<div class="input-container">
+                      <input type="text" id="entreEmail" required="required" />
+                      <label for="entreEmail">Email</label>
+                      <div class="bar"></div>
+                  </div>`;
+  var passwordHTML = `<div class="input-container">
+                        <input type="password" id="entrePassword" required="required" />
+                        <label for="entrePassword">Password</label>
                         <div class="bar"></div>
                       </div>`;
   var addressHTML = `<div class="input-container">
-                        <input type="text" id="address" />
-                        <label for="address">Address</label>
+                        <input type="text" id="entreAddress" />
+                        <label for="entreAddress">Address</label>
                         <div class="bar"></div>
                     </div>`;
   var websiteHTML = `<div class="input-container">
@@ -59,9 +59,23 @@ function createEntreRegistrationHTML() {
                         <label for="website">Website</label>
                         <div class="bar"></div>
                     </div>`;
+  var descriptionHTML = `<div class="input-container">
+                            <input type="text" id="description" />
+                            <label for="description">Description of company/organisation</label>
+                            <div class="bar"></div>
+                        </div>`
   var registerButton = `<div class="button-container">
-                            <button id="entre-register-go"><span>Register</span></button>
+                            <button onclick="entreRegister()"><span>Register</span></button>
                         </div>`;
 
-  return emailHTML + passwordHTML + addressHTML + websiteHTML + registerButton;
+  return emailHTML + passwordHTML + addressHTML + websiteHTML + descriptionHTML + registerButton;
+}
+
+function entreRegister() {
+  // calls the register api
+  // needs to watch out for
+}
+
+function patronRegister() {
+  // calls the register api
 }
