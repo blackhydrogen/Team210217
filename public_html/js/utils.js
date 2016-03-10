373e82ef81193f2afd55087ef37fe631f1c2162a
Leon Chin <leonchin499@gmail.com>
2016-03-10 21:40:43 +0800

    added utils stuff

--- public_html/js/utils.js
+++ public_html/js/utils.js
@@ -1,3 +1,29 @@
 function logout() {
-  
+  window.location.href = "/index.html";
+}
+
+function getUrlParameters(parameter, staticURL, decode) {
+  /*
+  Function: getUrlParameters
+  Description: Get the value of URL parameters either from
+  current URL or static URL
+  Author: Tirumal
+  URL: www.code-tricks.com
+  */
+  var currLocation = (staticURL.length) ? staticURL : window.location.search,
+  parArr = decodeURIComponent(currLocation).split("?")[1].split("&"),
+  returnBool = true;
+
+  for (var i = 0; i < parArr.length; i++) {
+
+    var parr = parArr[i].split("=");
+    if (parr[0] == parameter) {
+      return (decode) ? decodeURIComponent(parr[1]) : parr[1];
+      returnBool = true;
+    } else {
+      returnBool = false;
+    }
+  }
+
+  if (!returnBool) return false;
 }
