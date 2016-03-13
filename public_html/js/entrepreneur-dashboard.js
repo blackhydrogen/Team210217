38c9fe19590e01ac429210a8d92920b39903008f
Leon Chin <leonchin499@gmail.com>
2016-03-13 14:38:40 +0800

    added logout button

--- public_html/js/entrepreneur-dashboard.js
+++ public_html/js/entrepreneur-dashboard.js
@@ -1,5 +1,5 @@
 /*
- ProjectIDs for each item are defined as such: 
+ ProjectIDs for each item are defined as such:
  <title>-<email>
  */
 
@@ -35,10 +35,10 @@ function displayProjects(data) {
 
     var htmlToBeDisplayed = formatHtml(currentPage, projects);
     var paginationHTML = createPaginationHTML(currentPage, totalPage);
-    
+
     document.getElementById("projectBody").innerHTML = htmlToBeDisplayed;
     document.getElementById("pageHTML").innerHTML = paginationHTML;
-    
+
   } else {
     console.log(serverResponse.errorMessage);
   }
@@ -53,18 +53,24 @@ function formatHtml(currPage, projects) {
     displayNoProjects();
   } else {
     var entireHTML = "";
-    
+
     for (var i = 0; i < projects.length; i++) {
-      var html = createItemHtml(currPage, i, projects[i]);
-      
+      var html = createItemHtml(currPage, projects[i]);
+
       entireHTML = entireHTML + html;
     }
   }
-  
+
   return entireHTML;
 }
 
-function createItemHtml(page, itemNo, project) {
+function createItemHtml(page, project) {
+  var description = project.description;
+  if(description.length > 400) {
+    description = description.substring(0, 399);
+    description = description + "...";
+  }
+
   var html = `<div class="row">
             <div class="col-md-7">
                 <a href="portfolio-item.html">
@@ -74,76 +80,73 @@ function createItemHtml(page, itemNo, project) {
             <div class="col-md-5">
                 <h3>` + project.title + `</h3>
                 <h4>Goal: ` + project.raised + ` / ` + project.goal + `</h4>
-                <p>` + project.description + `</p>
+                <p>` + description + `</p>
                 <a class="btn btn-primary" onclick="goToProject('` + project.title + `', '` + project.email + `')">View Project</i></a>
             </div>
         </div><hr>`;
   return html;
 }
 
-  
+function createPaginationHTML(currPage, totPage) {
+  var leftStr = "";
+  var rightStr = "";
+  var centre = "";
 
-function createPaginationHTML(currPage, totPage){
-  if(totPage < 5) {
-    for(var i = 0; i < totPage; i++) {
-      var leftStr = "";
-      var rightStr = "";
-      
-      if(currPage == 1) {
-        leftStr = `<li>
-                        <a href="#">&laquo;</a>
-                    </li>`
-      } else {
-        leftStr = `<li>
-                        <a href="#" onclick=getAllProjects(`+ (currPage - 1) + `)>&laquo;</a>
-                    </li>`
-      }
-      
-      if(totPage == currPage) {
-        rightStr = `<li>
-                        <a href="#">&raquo;</a>
-                    </li>`
-      } else {
-        rightStr = `<li>
-                        <a href="#" onclick=getAllProjects(`+ (currPage + 1) + `)>&laquo;</a>
-                    </li>`
-      }
-      
-      var centre = "";
-      for(var i = 1; i <= totPage; i++) {
-        if(i == currPage) {
-          centre = centre + `<li class="active">
-                        <a>` + i + `</a>
-                    </li>`
-        } else {
-          centre = centre + `<li>
-                          <a href="#" onclick=getAllProjects(` + i + `)>` + i + `</a>
-                      </li>`
-        }
-      }
-      
-      return leftStr + centre + rightStr;
+  if(currPage > 1) {
+    leftStr = `<li>
+                    <a href="#" onclick=getAllProjects(`+ (currPage - 1) + `)>&laquo;</a>
+                </li>`;
+  }
+
+  if(currPage < totPage) {
+    rightStr = `<li>
+                    <a href="#" onclick=getAllProjects(`+ (currPage + 1) + `)>&laquo;</a>
+                </li>`;
+  }
+
+  if(totPage <= 5) {
+    for (var i = 1; i <= totPage; i++) {
+      centre = centre + createPageNumberHTML(i, currPage);
     }
   } else {
-    if(currPage >= 3) {
-      
+    if(currPage <= 3) {
+      for (var i = 1; i <= 5; i++) {
+        centre = centre + createPageNumberHTML(i, currPage);
+      }
+    } else if(currPage > 3 && currPage + 2 < totPage) {
+      for(var i = currPage - 2; i <= currPage + 2; i++) {
+        centre = centre + createPageNumberHTML(i, currPage)
+      }
+    } else {
+      for(var i = totPage - 4; i <= totPage; i++) {
+        centre = centre + createPageNumberHTML(i, currPage);
+      }
     }
-  }        
+  }
+
+  return leftStr + centre + rightStr;
 }
 
+function createPageNumberHTML(iter, currPage) {
+  if(iter == currPage) {
+    return `<li class="active">
+                <a>` + iter + `</a>
+            </li>`;
+  } else {
+    return `<li>
+                <a href="#" onclick=getAllProjects(` + iter + `)>` + iter + `</a>
+            </li>`;
+  }
+}
 
 function displayNoProjects() {
 
 }
 
 function goToProject(title, email) {
-  console.log(title);
-  console.log(email);
   var params = "title=" + title + "&email=" + email;
-  
+
   var html = "/secure/entre-project-detail.html?" + encodeURIComponent(params);
-  console.log(html);
-  console.log(decodeURIComponent(html));
-  
+
   window.location.href = html;
 }
