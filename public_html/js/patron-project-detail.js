78d9709df0dd992bffb4dae3a69253cb871cc43e
Ivan Tjahjono <Ivan Tjahjono>
2016-03-24 00:20:28 +0800

    added tags for patron-project-detail.js

--- public_html/js/patron-project-detail.js
+++ public_html/js/patron-project-detail.js
@@ -4,24 +4,40 @@ function runOnLoad() {
 }
 
 function getProjectDetails() {
-    var title = getUrlParameters("title", "", true);
-    var email = getUrlParameters("email", "", true);
+    //var title = getUrlParameters("title", "", true);
+    //var email = getUrlParameters("email", "", true);
 
-    $.post({
-        url: "/getProjectDetails",
-        data: JSON.stringify({
-            title: title,
-            email: email
-        }),
-        success: function (data, response) {
-            if (response == "success") {
-                displayProject(data);
-            } else {
-                connectionError(response);
-            }
-        },
-        contentType: "application/json"
-    });
+    // $.post({
+    //     url: "/getProjectDetails",
+    //     data: JSON.stringify({
+    //         title: title,
+    //         email: email
+    //     }),
+    //     success: function (data, response) {
+    //         if (response == "success") {
+    //             displayProject(data);
+    //         } else {
+    //             connectionError(response);
+    //         }
+    //     },
+    //     contentType: "application/json"
+    // });
+
+    var data ={
+      success: true,
+      errorMessage: "error message",
+      title: "Test Project 1",
+			description: "Test Project 1 description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+			goal: 10000.0,
+			raised: 150.26,
+			start: new Date("26 Feb 2016 23:59:00 GMT+0800").getTime(),
+			end: new Date("15 Mar 2016 10:00:00 GMT+0800").getTime(),
+			tags: "test, project, testproject",
+			email: "test@gmail.com",
+			name: "Atul NK"
+    };
+
+    displayProject(JSON.stringify(data));
 
 }
 
@@ -36,12 +52,23 @@ function displayProject(data) {
 			<li>Raised: ${response.raised}</li>
 			<li>Start Time: ${new Date(response.start).toLocaleString()}</li>
 			<li>End Time: ${new Date(response.end).toLocaleString()}</li>
-			<li>Tags: ${response.tags}</li>
+			<li>Tags:` + createTags(response.tags) + `</li>
 		`)
 	}
 	else {
 		alert(response.errorMessage);
 	}
+}
+
+function createTags(tags){
+  var arrayTags = tags.trim("").split(",");
+  var html="";
+
+  for(var i=0;i<arrayTags.length;i++){
+    html = html+`<button type="button" class="btn btn-info btn-sm">`+ arrayTags[i] +`</button>`;
+  }
+
+  return html;
 }
 
 function fundmenow() {
